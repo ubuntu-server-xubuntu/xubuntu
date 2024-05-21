6b20700a64b9e8505e0a21f073722edbd3a35fe5
ubuntu-server-xubuntu <ubuntu-server-xubuntu@proton.me>
2024-05-21 17:13:23 -0300

    update install.sh

--- server-install.sh
+++ server-install.sh
@@ -8,8 +8,8 @@ gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
 sudo apt update && sudo apt upgrade -y && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade && sudo apt autoremove -y && sudo apt-get autoremove -y && sudo snap refresh && sudo fwupdmgr refresh && sudo fwupdmgr get-updates -y && sudo fwupdmgr update
 # configura o ambiente para iniciar o instalador depois de baixar o ubuntu-server 
 sudo chmod +x ~/xubuntu-heads-main/install.sh
-mkdir /home/$USER/.config/autostart/
-cp ~/xubuntu-heads-main/xubuntu.desktop /home/$USER/.config/autostart/
+sudo mkdir /home/$USER/.config/autostart/
+sudo cp ~/xubuntu-heads-main/xubuntu.desktop /home/$USER/.config/autostart/
 sudo chmod +x /home/$USER/.config/autostart/xubuntu.desktop
 # instala o ubuntu-server
 echo "gdm3 shared/default-x-display-manager select gdm3" | sudo debconf-set-selections
